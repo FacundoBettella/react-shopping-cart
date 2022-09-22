817a1035f36f6daa5b263073fbeb7bc60868f4cb
SA\TV126SH <facundo.bettella@ar.ey.com>
2022-09-22 15:33:57 -0300

    Se agregan product assets

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Fragment } from "react";
 import { GlobalStyle } from "./styles/GlobalStyles";
 import { Title } from "./components";
+import AppleIpad from "./assets/Apple iPad.jpg";
+// import { getData } from "./firebase/crud/firebaseCrudExample";
 
 const App = () => {
+  // let data = getData("products");
+
   return (
     <Fragment>
       <GlobalStyle />
       <Title text={"Shopping Chart"} />
       {/* routes */}
+      <div>
+        <img src={AppleIpad} style={{ width: "230px" }} />
+      </div>
     </Fragment>
   );
 };
--- src/firebase/Crud/firebaseCrudExample.js
+++ src/firebase/Crud/firebaseCrudExample.js
@@ -1,20 +1,20 @@
 import { collection, addDoc, getDocs } from "firebase/firestore";
 import { FIRESTONE } from "../firebase.config";
 
-export const addData = async (user) => {
+export const addData = async (entity, object) => {
   try {
-    const dbResponse = await addDoc(collection(FIRESTONE, "user"), user);
+    const dbResponse = await addDoc(collection(FIRESTONE, entity), object);
     console.log("Document written with ID: ", dbResponse.id);
   } catch (e) {
     console.error("Error adding document: ", e);
   }
 };
 
-export const getData = async () => {
+export const getData = async (entity) => {
   try {
-    const usersResponse = await getDocs(collection(FIRESTONE, "user"));
-    usersResponse.forEach((doc) => {
-      console.log(`${doc.id} => ${doc.data().name}`);
+    const dataResponse = await getDocs(collection(FIRESTONE, entity));
+    dataResponse.forEach((doc) => {
+      console.log(`${doc.data().title}`);
     });
   } catch (e) {
     console.error("Error getting document: ", e);
