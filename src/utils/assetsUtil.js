244022dab9ea7e7296ab07c821223f3d248bb4cf
facundobettella <facundobettella@outlook.com>
2022-09-26 14:38:35 -0300

    Se crea useProducts

--- src/utils/assetsUtil.js
+++ src/utils/assetsUtil.js
@@ -1,7 +1,22 @@
-const sommierImg = require("../assets/Sommier Piero Foam.jpg");
+const aireAcondicionado = require("../assets/Aire acondicionado.jpg")
 const appleIpadImg = require("../assets/Apple iPad.jpg");
+const cortina = require("../assets/Cortina Blackout.jpg");
+const dockStation = require("../assets/Dock Station Dell.jpg");
+const escritorio = require("../assets/Escritorio Su-Office .jpg");
+const heladera = require("../assets/Heladera Whirlpool.jpg");
+const lamapara = require("../assets/Lampara.jpg");
+const lavarropas = require("../assets/Lavarropas automático.jpg");
+const microondas = require("../assets/Microondas Tedge.jpg");
+const monopatin = require("../assets/Monopatin Electrico.jpg");
+const nintendoSwitch = require("../assets/Nintendo Switch.jpg");
+const notebook = require("../assets/Notebook Lenovo.jpg");
+const cell = require("../assets/Samsung A03.jpg");
+const Essen = require("../assets/Set 4x3 Essen Flip.jpg");
+const silla = require("../assets/Silla Escritorio.jpg");
+
+const sommierImg = require("../assets/Sommier Piero Foam.jpg");
 const bicicleta = require("../assets/Bicicleta.jpg");
-const lavarropas = require("../assets/Lavarropas automático.jpg")
+
 const notFound = require("../assets/not found.jpg");
 
 export const assetsUtils = [
@@ -21,6 +36,15 @@ export const assetsUtils = [
         name: "Lavarropas automático",
         src: lavarropas
     },
+    {
+        name: "Aire acondicionado",
+        src: aireAcondicionado
+    },
+    {
+        name: "Cortina Blackout",
+        src: cortina
+    },
+
     {
         name: "Not Found",
         src: notFound
