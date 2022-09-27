f0525e6c7d7352d590a8594eda5eb6312ef66c43
facundobettella <facundobettella@outlook.com>
2022-09-27 16:45:03 -0300

    Se resuelven conflictos

--- src/components/index.js
+++ src/components/index.js
@@ -2,11 +2,7 @@ import Title from "./title";
 import Carousel from "./carousel";
 import Products from "./products";
 import Loading from "./loading";
-<<<<<<< Updated upstream
-
-export { Title, Carousel, Products, Loading };
-=======
 import ChangeAlertWithStorageListener from "./changeStorageAlert/index";
 
 export { Title, Carousel, Products, Loading, ChangeAlertWithStorageListener };
->>>>>>> Stashed changes
+
