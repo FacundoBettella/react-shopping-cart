ff732977e363805c7b430de35c3efc50443566d9
facundobettella <facundobettella@outlook.com>
2022-10-02 14:42:28 -0300

    Se crea branch logica de login

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,13 @@ import Carousel from "./carousel";
 import Products from "./products";
 import Loading from "./loading";
 import ChangeAlertWithStorageListener from "./changeStorageAlert/index";
+import { Navbar } from "./navbar/navbar";
 
-export { Title, Carousel, Products, Loading, ChangeAlertWithStorageListener };
-
+export {
+  Title,
+  Carousel,
+  Products,
+  Loading,
+  ChangeAlertWithStorageListener,
+  Navbar,
+};
