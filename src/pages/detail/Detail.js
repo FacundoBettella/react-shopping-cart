4b543790ca87610dbf4df498e5a4e8c2d4f2d868
Juan Marderwald <jmarderwald87@gmail.com>
2022-10-04 13:16:36 -0300

    Agrego texto para cuotas, envio y retiro en sucursal

--- src/pages/detail/Detail.js
+++ src/pages/detail/Detail.js
@@ -1,21 +1,39 @@
 import React, { Fragment, useContext } from "react";
 import { useLocation } from "react-router-dom";
-import { BotonAgregarCarrito, DescriptionContainer, DetailsContainer, DetailsImage, LargeDescription, ProductAvailable, ProductPrice, ProductTitle } from "./styles";
+import { BotonAgregarCarrito, ContenedorEnvio, ContenedorEnvioSucursal, CuotasSinInteres, DescriptionContainer, DetailsContainer, DetailsImage, LargeDescription, ProductAvailable, ProductPrice, ProductTitle } from "./styles";
+import { RiStore2Line } from "react-icons/ri";
+import { MdLocalShipping } from "react-icons/md";
+
+
 
 const Detail = () => {    
     const location = useLocation();
-    const currentProduct = location.state.product;
+    const selectedProduct = location.state.product;
     
+    const handleClick = () =>{
+        console.log("Se agrega al carrito");
+    }
+
+    function redondearCuotas() {
+        return (selectedProduct.price/6).toPrecision(4);
+    }    
+
     return (
         <Fragment>
-            <ProductTitle> {currentProduct.title} </ProductTitle>
+            <ProductTitle> {selectedProduct.title} </ProductTitle>
             <DetailsContainer>
-                <DetailsImage src={require(`../../assets/${currentProduct.title}.jpg`)} alt={`${currentProduct.title}`} />
+                <DetailsImage src={require(`../../assets/${selectedProduct.title}.jpg`)} alt={`${selectedProduct.title}`} />
                 <DescriptionContainer>
-                    <LargeDescription>{currentProduct.largDescription}</LargeDescription>
-                    <ProductAvailable>{currentProduct.stock} disponibles</ProductAvailable>
-                    <ProductPrice>$ {currentProduct.price}</ProductPrice>
-                    <BotonAgregarCarrito>AGREGAR AL CARRITO</BotonAgregarCarrito>
+                    <LargeDescription>{selectedProduct.largDescription}</LargeDescription>
+                    <ProductAvailable>{selectedProduct.stock} disponibles</ProductAvailable>
+                    <ProductPrice>$ {selectedProduct.price}</ProductPrice>
+                    <CuotasSinInteres>6 Cuotas sin interés de ${redondearCuotas()}</CuotasSinInteres>
+                    <ContenedorEnvioSucursal> 
+                        <p> <MdLocalShipping /> Envío gratis a Capital Federal </p>
+                        <p> Llega mañana comprando hoy antes de las 19hs</p>
+                     </ContenedorEnvioSucursal>
+                    <ContenedorEnvioSucursal><RiStore2Line/> Retiro Gratis en sucursal</ContenedorEnvioSucursal>
+                    <BotonAgregarCarrito onClick={handleClick}>AGREGAR AL CARRITO</BotonAgregarCarrito>
                 </DescriptionContainer>
             </DetailsContainer>            
         </Fragment>
--- src/pages/detail/styles.js
+++ src/pages/detail/styles.js
@@ -5,22 +5,23 @@ export const DetailsImage = styled.img`
     height: 500px;
 `
 
-export const DescriptionContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-`
-
 export const DetailsContainer = styled.div`
     display: flex;
     padding: 5px 5px;
-    border: 2px solid black;
+    
 `
+
+export const DescriptionContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin:0px 20px;
+`
+
 export const ProductTitle = styled.h1`
     margin: 20px 10px;
 `
 
-export const LargeDescription = styled.span`
-    margin:0px 10px;
+export const LargeDescription = styled.span`    
     font-size: 25px;
 `
 
@@ -29,17 +30,28 @@ export const ProductPrice = styled.h2`
     font-size: 30px;
 `
 
+export const CuotasSinInteres = styled.p`
+    margin: 20px 0;
+    font-size: 20px;
+`
+
 export const ProductAvailable = styled.h3`
     margin: 20px 10px;
     font-size: 20px;
 `
+export const ContenedorEnvioSucursal = styled.div`
+    font-size: 20px;
+    margin: 10px 0;
+`
+
 export const BotonAgregarCarrito = styled.button`
-  font-size: 15px;
+  font-size: 20px;
   background-color: grey;
-  height: 70px;
-  width: 200px;
+  font-weight:bold;
+  height: 70px;  
   box-shadow: 0 10px 14px rgba(0, 0, 0, 0.2);
   border-radius: 5px;
-  margin: 10% 40%;
+  margin: 5% 40%;
+  padding: 10px;
   cursor: pointer;
 `
