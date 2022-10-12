5cbfd3a0aa74a4ff1895979947e6d944641536d2
facundobettella <facundobettella@outlook.com>
2022-10-12 16:56:17 -0300

    Se integra T6-13_ComponenteCarrito

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -4,8 +4,14 @@ import { doc, updateDoc } from "firebase/firestore";
 import { Button } from "../../components";
 import swal from "sweetalert";
 import { carritoContext } from "../../context/carritoContext";
-import { CartItemsContainer, CartTotal, TotalText, TotalPrice, VacioText } from "./styles";
-import {CartItem } from './cartItem/CartItem'
+import {
+  CartItemsContainer,
+  CartTotal,
+  TotalText,
+  TotalPrice,
+  VacioText,
+} from "./styles";
+import { CartItem } from "./cartItem/CartItem";
 
 const Cart = () => {
   // const { currentChartArticles } = useChart();
@@ -42,21 +48,30 @@ const Cart = () => {
   return (
     <>
       <CartItemsContainer>
-        {carrito.map((article,i) => (
-          <CartItem key={article.title + i} article={article}/>
+        {carrito.map((article, i) => (
+          <CartItem key={article.title + i} article={article} />
         ))}
-        
+
         <CartTotal>
-        {
-          tamañoCarrito() === 0 ? <VacioText>El carrito está vacío!</VacioText> :
-          <>
-            <TotalText>Total</TotalText>
-            <TotalPrice>{parseFloat(carrito.reduce((partialSum, a) => parseFloat(partialSum) + parseFloat(a.price), parseFloat(0))).toFixed(2)}</TotalPrice>
-          </>
-        }
+          {tamañoCarrito() === 0 ? (
+            <VacioText>El carrito está vacío!</VacioText>
+          ) : (
+            <>
+              <TotalText>Total</TotalText>
+              <TotalPrice>
+                {parseFloat(
+                  carrito.reduce(
+                    (partialSum, a) =>
+                      parseFloat(partialSum) + parseFloat(a.price),
+                    parseFloat(0)
+                  )
+                ).toFixed(2)}
+              </TotalPrice>
+            </>
+          )}
         </CartTotal>
       </CartItemsContainer>
-      
+
       <Button onClick={handleBuyArticle}>Confirmar compra</Button>
     </>
   );
