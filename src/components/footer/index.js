e7e07b2151312734f8e010bd048d167aa88888d4
Juan Ignacio Marderwald <jmarderwald87@gmail.com>
2022-10-17 12:29:43 -0300

    Se agrega las imagenes del logo en el footer y en el Nav

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,31 +1,33 @@
 import React, { Fragment } from "react";
-import { ContenedorFooter, DerechosContainer, InfoContainer, Linea, LinksContainer, SocialContainer, LegalesContainer } from "./styles";
+import { ContenedorFooter, DerechosContainer, InfoContainer, Columna, Logo } from "./styles";
 import { SiInstagram, SiFacebook, SiLinkedin, SiGmail } from "react-icons/si";
-
+import { Link } from "react-router-dom";
+import logoAlt from "../../assets/logo/shopping_alt.png"
 export const Footer = () => {
   return (
     <ContenedorFooter>
      
       <InfoContainer>
-        <SocialContainer>
+        <Logo src={logoAlt} alt="logo"></Logo>
+        <Columna>
           <p> Seguinos:  <SiGmail />  <SiInstagram /> <SiFacebook /> <SiLinkedin /> </p>      
-        </SocialContainer>
+        </Columna>
 
-        <LegalesContainer>
+        <Columna>
           <h3>LEGALES</h3>
           <p>Términos y Condiciones</p> 
           <p>Políticas de Privacidad</p>    
           <p>Políticas de Cambio</p>    
-          <p>Defensa de las y los Consumidores Para reclamos ingrese aquí</p>     
-        </LegalesContainer>
+          <p>Defensa de las y los Consumidores Para reclamos ingrese <a href="https://autogestion.produccion.gob.ar/consumidores" target="_blank" rel="noopener noreferrer">aquí</a></p>     
+        </Columna>
 
-        <LinksContainer>
+        <Columna>
           <h3>INSTITUCIONAL</h3>
           <p>Contacto</p>
           <p>La empresa</p>
           <p>Nuestra gente</p>
           <p>Trabaja con nosotros</p>
-        </LinksContainer>
+        </Columna>
       
       </InfoContainer>
       <DerechosContainer>
